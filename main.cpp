951abd108b1971af501d3c7b53f4a391158bf59b
DoubleProgram <92651727+DoubleProgram@users.noreply.github.com>
2021-12-27 12:43:23 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "generation.hpp"
+#include <chrono>
+
+int main() {
+    auto duration = std::chrono::system_clock::now().time_since_epoch();
+    auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+
+    srand(millis);
+    Generation gen(128,128,4,4,4,4);
+    if(gen.Start(200,6,10,6,10) == -1)
+        std::cout << "ERROR: The rooms can't fit into the map, decrease the minimum rooms size or the amount" << std::endl;
+    else
+        for(int j = 0; j < gen.HEIGHT; j++)
+            std::cout << gen.map[j] << std::endl;
+    return 0;
+}
